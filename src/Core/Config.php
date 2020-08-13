e7c6117d34aadcfa7dad7bf31de5635f0324fa25
Dzmitry2020 <pupkinvas378@gmail.com>
2020-08-13 17:29:55 +0300

    Update Config.php

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -9,7 +9,7 @@ class Config
     const MYSQL_PASSWORD = 'root';
     const MYSQL_DATABASE = 'mytest';
     const PAGE_SIZE = 5;
-    const QUESTIONS_COUNT = 5;
+    const QUESTIONS_COUNT = 20;
     const TIME_ZONE = "Europe/Minsk";
     public const SALT = 'my_secret_word';
 
